ebe8c9964fba8dd7f62e2b95929432ddac47b1f6
Cameron Kleiman <camtheman256@gmail.com>
2021-04-15 18:54:12 +0000

    Fix app props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { AppProps } from 'next/app'
 import { ThemeProvider } from 'theme-ui'
 import theme from '../styles/theme'
 
