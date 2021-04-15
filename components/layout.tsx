896eadbb50c902eea10769d6525d3110e9420bbe
Cameron Kleiman <camtheman256@gmail.com>
2021-04-14 22:23:01 -0500

    set up profile

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
-import { Box, Link } from 'theme-ui'
+import { Avatar, Box, Container, Flex, Heading, Link } from 'theme-ui'
 
-function Header() {
+function HeadBar() {
   const upTo = [
     'making a website',
     'up too late again',
@@ -15,16 +15,32 @@ function Header() {
 
   return (
     <header>
-      <Box sx={{p: 3, color: 'muted'}}>Cameron Kleiman is {upTo[Math.floor(Math.random()*upTo.length)]}</Box>
+      <Box sx={{p: 3, color: 'muted', fontSize: 'body'}}>Cameron Kleiman is {upTo[Math.floor(Math.random()*upTo.length)]}</Box>
     </header>
+  )
+}
+
+function Header() {
+  return (<>
+    <Flex sx={{alignItems: 'center', flexDirection: ['column', 'row']}} mb={4}>
+      <Avatar src="profile.jpg" mr={[0, 4]} mb={[3,0]} sx={{width: [125, 125, 150]}} />
+      <Box sx={{textAlign: ['center', 'left']}}>
+        <Heading sx={{fontSize: ['subheading', 'display-2', 'display']}}>Cameron Kleiman</Heading>
+        <Heading sx={{color: 'accent'}}>computer science &amp; planning student at mit</Heading>
+      </Box>
+    </Flex>
+    </>
   )
 }
 
 export default function Layout({ children }) {
   return (
     <>
-      <Header />
-      {children}
+      <HeadBar />
+      <Container p={[3, 4]}>
+        <Header />
+        {children}
+      </Container>
     </>
   )
 }
--- styles/theme.ts
+++ styles/theme.ts
@@ -5,23 +5,47 @@ const theme: Theme = {
     colors: {
         text: '#000',
         background: '#fff',
+        //TODO: make light mode color scheme
         primary: 'red',
-        muted: 'grey',
+        secondary: '',
+        muted: '#444',
         modes: {
             dark: {
                 text: '#fff',
-                background: '#000'
+                background: '#111',
+                primary: '#F08080',         // light coral
+                secondary: '#63D2FF',       // baby blue
+                accent: '#37FF8B',          // spring green
+                muted: '#ccc',
             }
         }
     },
     fonts: {
         body: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+        heading: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
     },
+    fontSizes: {
+        small: 12,
+        body: 16,
+        large: 24,
+        subheading: 32,
+        title: 48,
+        "display-2": 54,
+        display: 72,
+    },
+    breakpoints: [
+        '40em', '54em', '62em'
+    ],
     styles: {
         root: {
             fontFamily: 'body',
             fontWeight: 'body'
         },
+    },
+    layout: {
+        container: {
+            maxWidth: [ '40em', '44em', '54em' ] ,
+        }
     },
 }
 
