bff0c3744c46286a18f3d90fd9ba6c0a1d3a30ea
Cameron Kleiman <camtheman256@gmail.com>
2021-04-18 03:57:40 +0000

    write slidetip component

---
+++ components/slidetip.tsx
@@ -0,0 +1,38 @@
+/** @jsxImportSource theme-ui */
+import { Box } from "@theme-ui/components"
+import { darken, lighten } from "@theme-ui/color"
+import { useState } from 'react'
+import { useThemeUI } from "@theme-ui/core";
+
+export default function SlideTip({ children, title }) {
+    const [ state, setState ] = useState({
+        showTip: false
+    });
+
+    const { colorMode } = useThemeUI()
+
+    const tipTheme = 'primary'
+
+    return (<span onClick={() => setState({showTip: !state.showTip})} sx={{
+        cursor: 'pointer' 
+    }}>
+        <Box sx={{
+            display: 'inline',
+            backgroundColor: tipTheme,
+            color: 'background',
+            p: '1px 3px',
+            borderRadius: state.showTip ? '2px 0px 0px 2px' : '2px',
+        }}>
+            { children }
+        </Box>
+        {state.showTip && <Box sx={{
+            display: 'inline',
+            backgroundColor: colorMode == 'light' ? darken(tipTheme, 0.1) : lighten(tipTheme, 0.1),
+            color: 'background',
+            p: '1px 3px',
+            borderRadius: '0px 2px 2px 0px',
+        }}>
+            { title }
+        </Box>}
+    </span>)
+}
