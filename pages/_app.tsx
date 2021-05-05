ef81b3c95150968167a3c416ecad678ac7d093c9
Cameron Kleiman <camtheman256@gmail.com>
2021-05-05 04:57:24 +0000

    Set up ganalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import { ThemeProvider } from 'theme-ui'
+import { pageview } from '../lib/ga'
 import theme from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <ThemeProvider theme={theme}> 
     <Component {...pageProps} />
   </ThemeProvider>
---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { G_MEASUREMENT_ID } from '../lib/ga'
+import { InitializeColorMode } from 'theme-ui'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${G_MEASUREMENT_ID}`} />
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${G_MEASUREMENT_ID}');
+            `
+          }} />
+        </Head>
+        <body>
+          <InitializeColorMode />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
