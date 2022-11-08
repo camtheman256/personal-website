9e636f335bbf4a77adcb09721cbef888b31c17b4
Cameron Kleiman <camtheman256@gmail.com>
2022-11-08 16:36:57 -0500

    make mdx work good

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,34 @@
-import { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import { ThemeProvider } from 'theme-ui'
-import { pageview } from '../lib/ga'
-import theme from '../styles/theme'
+import { MDXProvider, useMDXComponents } from "@mdx-js/react";
+import { useThemedStylesWithMdx } from "@theme-ui/mdx";
+import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { ThemeProvider } from "theme-ui";
+import { pageview } from "../lib/ga";
+import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
+  const router = useRouter();
+  const componentsWithStyles = useThemedStylesWithMdx(useMDXComponents());
 
   useEffect(() => {
     const handleRouteChange = (url) => {
-      pageview(url)
-    }
+      pageview(url);
+    };
 
-    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
-  return <ThemeProvider theme={theme}> 
-    <Component {...pageProps} />
-  </ThemeProvider>
+  return (
+    <ThemeProvider theme={theme}>
+      <MDXProvider components={componentsWithStyles}>
+        <Component {...pageProps} />
+      </MDXProvider>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
